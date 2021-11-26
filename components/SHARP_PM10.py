9b60f0593f5f304e39e9e29df9153f896a1d15a4
CxrlosKenobi <kenobi@ieee.org>
2021-11-25 23:35:47 -0300

    Fix typo

--- components/SHARP_PM10.py
+++ components/SHARP_PM10.py
@@ -3,7 +3,7 @@ from serial import Serial
 
 class SharpPM10:
     def __init__(self):
-        self.serial = Serial('/dev/ttyAMA0', 9600, timeout=1)
+        self.serial = Serial('/dev/ttyS0', 9600, timeout=1)
 
     def read(self):
         with self.serial as ino:

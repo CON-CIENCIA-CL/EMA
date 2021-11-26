37d0f108bd8c399cca65d33f6750e1d2472eb1fe
CxrlosKenobi <kenobi@ieee.org>
2021-11-25 23:24:11 -0300

    Refactor Sharp and Main code

---
+++ components/tests/HDC_1080-test.py
@@ -0,0 +1,26 @@
+import sys
+from components.setups import SDL_Pi_HDC1080
+
+class HDC:
+    def __init__(self):
+        sys.path.append('./modules/SDL_Pi_HDC1080_Python3')
+        self.hdc1080 = SDL_Pi_HDC1080.SDL_Pi_HDC1080()
+
+    def temp(self):
+        temperature = self.hdc1080.readTemperature()
+        return temperature
+
+    def hum(self):
+        humidity = self.hdc1080.readHumidity()
+        return humidity
+        
+    def read(self, decimal):
+        temp = self.temp(decimal)
+        hum = self.hum(decimal)
+        return temp, hum
+
+HDC = HDC()
+
+data = {
+    'Temperature': 
+}

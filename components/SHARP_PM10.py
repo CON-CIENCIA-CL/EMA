07951439b1e1473ef5241c6672d1c0bc3546167f
CxrlosKenobi <kenobi@ieee.org>
2021-11-25 23:34:46 -0300

    Fix Sharp serial

--- components/SHARP_PM10.py
+++ components/SHARP_PM10.py
@@ -1,4 +1,4 @@
-from numpy import median
+from time import sleep
 from serial import Serial
 
 class SharpPM10:
@@ -7,10 +7,13 @@ class SharpPM10:
 
     def read(self):
         with self.serial as ino:
+            sleep(0.1)
             if ino.isOpen():
                 if ino:
                     pckt = ino.readline().decode('utf-8').rstrip()
+                    ino.flushInput()
                     if pckt:
                         return pckt
-            ino.flushInput()
+            else:
+                return '--'
 
